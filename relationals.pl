288b0f673c545a4747db659c60362ee9163709de
Saul Tobias <saul.tobias@fdmgroup.com>
2020-04-14 19:24:29 -0500

    fixed local variable issue

--- relationals.pl
+++ relationals.pl
@@ -1,9 +1,11 @@
 #!/usr/bin/perl
 
+use strict;
 use feature "switch";
 use v5.14;
 
-$a = 10;
+my $a = 10;
+my $stas;
 
 if( $a < 20 ) {
     print("$a is less than 20\n");
@@ -25,11 +27,15 @@ unless ( $a == 10 ) {
 
 print("end of unless\n");  
 
-
+print("\nBefore given statement\n");
 given($a) {
     when(10) { print("a is 10\n"); }
     when("a") { print("a is a\n");  }
     default { print("cases not true");  }
 }
 
+print("\nAfter given statement\n");
 
+$stas = ($a > 60)? "a is greater than 60" : "a is less than 60";
+
+print($stas\n);
